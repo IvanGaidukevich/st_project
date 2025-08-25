a5bf8978dec0b3469bafdb79bc3fc11529d6c41f
IvanGaidukevich <gaydu7@gmail.com>
2025-08-25 13:04:21 +0300

    add forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("blog:post_list")  # тут поменяем на login
+    else:
+        form = UserCreationForm()
+        return render(request,
+                      template_name='registration/registration.html',
+                      context={'form': form})
